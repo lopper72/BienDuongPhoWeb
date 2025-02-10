a06439e22b5c95bd19642e9d0200bc6f0398d434
lopper72 <vanphuong.fit@gmail.com>
2025-02-10 17:10:56 +0700

    app

--- resources/views/livewire/admin/product/add-product.blade.php
+++ resources/views/livewire/admin/product/add-product.blade.php
@@ -308,9 +308,6 @@
                 .create( document.querySelector( '#product_description' ),{
                     ckfinder: {
                         uploadUrl: '{{route('admin.ck-upload-image').'?_token='.csrf_token()}}'
-                    },
-                    mediaEmbed: {
-                        previewsInData: true
                     }
                 } )
                 .then(editor => {
