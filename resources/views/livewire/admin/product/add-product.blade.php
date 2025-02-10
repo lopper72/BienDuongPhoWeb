759316630dec5e55ee7f09f046d6f94eeea5d5b2
lopper72 <vanphuong.fit@gmail.com>
2025-02-10 09:55:01 +0700

    Add menlayout

--- resources/views/livewire/admin/product/add-product.blade.php
+++ resources/views/livewire/admin/product/add-product.blade.php
@@ -308,6 +308,9 @@
         .create( document.querySelector( '#product_description' ),{
             ckfinder: {
                 uploadUrl: '{{route('admin.ck-upload-image').'?_token='.csrf_token()}}'
+            },
+            mediaEmbed: {
+                previewsInData: true
             }
         } )
         .then(editor => {
